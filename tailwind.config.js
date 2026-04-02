29cdcb2b645b96242eb3d1cd0448d80181292d69
vbsbeatle <vbsbeatle@gmail.com>
2026-04-02 15:42:29 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'pesca-amarelo': '#FACC15',
+        'pesca-preto': '#000000',
+        'pesca-cinza-claro': '#F3F4F6',
+        'pesca-cinza-escuro': '#374151',
+      },
+    },
+  },
+  plugins: [],
+}
