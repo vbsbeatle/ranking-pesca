e655f3dec053f06e1ea774951bdd40e60cad87c6
vbsbeatle <vbsbeatle@gmail.com>
2026-04-02 16:33:46 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,4 @@
-import { supabase } from '../../lib/supabase';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
