bc7c1a536733df88b4a07f74a1d6702c1d9b50e7
vbsbeatle <vbsbeatle@gmail.com>
2026-05-13 16:03:25 -0300

    Update page.tsx

--- app/campeonatos/admin/page.tsx
+++ app/campeonatos/admin/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react'
-import { supabase } from '../../../lib/supabase'
+import { supabase } from '@/lib/supabase'
 
 export default function AdminCampeonatos() {
   const [campeonatos, setCampeonatos] = useState<any[]>([])
