ada2b8009117acf2b09aca63e1d25832882dd111
vbsbeatle <vbsbeatle@gmail.com>
2026-05-13 16:05:28 -0300

    Update page.tsx

--- app/pescador/[nome]/page.tsx
+++ app/pescador/[nome]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
-import { supabase } from '../../lib/supabase'
+import { supabase } from '../../../lib/supabase'
 
 export default function PerfilPescador() {
   const { nome } = useParams()
