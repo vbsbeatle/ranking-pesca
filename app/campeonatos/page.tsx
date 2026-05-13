0522bbe1b87554af94a8503bafe59290a875018a
vbsbeatle <vbsbeatle@gmail.com>
2026-05-13 15:06:52 -0300

    Update page.tsx

--- app/campeonatos/page.tsx
+++ app/campeonatos/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import { supabase } from '../lib/supabase'
+import { supabase } from '../../lib/supabase'
 
 export default function ListaCampeonatos() {
   const [torneios, setTorneios] = useState<any[]>([])
