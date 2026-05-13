9c05b1025ee80d1d8855e1af146b135faeb55304
vbsbeatle <vbsbeatle@gmail.com>
2026-05-13 15:12:01 -0300

    Update page.tsx

--- app/campeonatos/[id]/page.tsx
+++ app/campeonatos/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
-import { supabase } from '../../lib/supabase'
+import { supabase } from '../../../lib/supabase'
 
 export default function DetalheCampeonato() {
   const { id } = useParams()
